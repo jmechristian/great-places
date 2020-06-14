8ffb572ab01fa35cbd4cdaa35a47e31b8e8408c5
JMEChristian Digital <jmechristian@gmail.com>
2020-06-13 21:56:06 -0400

    forms

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
 import './NewPlace.css';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from '../../shared/Utils/validators';
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className='place-form'>
-      <Input type='text' label='Title' element='input' />
+      <Input
+        id='title'
+        type='text'
+        label='Title'
+        element='input'
+        errorText='Please enter a valid title.'
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={titleInputHandler}
+      />
+      <Input
+        id='description'
+        label='Description'
+        element='textarea'
+        errorText='Please enter a valid description (at least 5 characters).'
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        onInput={descriptionInputHandler}
+      />
     </form>
   );
 };
